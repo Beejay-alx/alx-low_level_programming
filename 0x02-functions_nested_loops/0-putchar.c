92509d71645adac78d1576bef05d5d3a9b48bb37
Beejay-alx <ibolaji20@gmail.com>
2023-07-19 02:09:06 -0700

    coding

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - this indicates the entry point of the program
+ *
+ * Return: return the value of the program
+ *
+ */
+int main(void)
+{
+	/* uses the putchar function to print to stdio */
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
