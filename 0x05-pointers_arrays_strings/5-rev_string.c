4ea6392112a3df02dabe4313bbccdf0490ac929e
Beejay-alx <ibolaji20@gmail.com>
2023-07-24 08:29:32 -0700

    coding

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,4 +2,21 @@
 #include <string.h>
 #include "main.h"
 /**
- * rev_string - print string in re
+ * rev_string - print string in reverse order
+ * @s: string stored
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	j = strlen(s) - 1;
+	for (i = 0; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+
+}
