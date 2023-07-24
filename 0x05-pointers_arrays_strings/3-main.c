d40fa630cc1c39996477f21362d4f10224420837
Beejay-alx <ibolaji20@gmail.com>
2023-07-24 06:06:29 -0700

    coding

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mainn - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computer. i fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
