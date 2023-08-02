d615289203d136dafbaf38fec5336c9f2457f7bb
Beejay-alx <ibolaji20@gmail.com>
2023-08-02 01:40:33 -0700

    coding

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @string: string
+ * @s: string
  * Return: no return
  */
 void _puts_recursion(char *s)
@@ -13,5 +13,5 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+		_putchar ('\n');
 }
