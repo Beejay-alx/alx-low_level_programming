df1f84adb26ec8bbe3b26fca4b26ca8186c57f24
Beejay-alx <ibolaji20@gmail.com>
2023-08-11 03:13:58 -0700

    coding

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
-char *string_noncat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *reallloc(void *ptr, unsigned int old_size, unsigned int new_size);
