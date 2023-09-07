16b2017f3bbf2d1293faa69d9f48aefc36b8485c
Beejay-alx <ibolaji20@gmail.com>
2023-09-07 09:36:44 -0700

    coding

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
  * read_textfile - reads a text file and prints the letters
  * @filename: filename
  * @letters: numbers of letters printed
- * Return: numbers of letters printed. it fails, return 0
+ *
+ * Return: numbers of letters printed. it fails, returns 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -14,12 +15,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd == 1)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
@@ -30,6 +31,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
+
 	free(buf);
+
 	return (nwr);
 }
